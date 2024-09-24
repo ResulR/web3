6fb501561802bb60e300a1a5d8d48eff7800c423
Resul <resul.ramadani@student.vinci.be>
2024-09-24 15:27:04 +0200

    new:ex2.1-events

--- Exercices/Module2/Exercices/2.1/src/clickCounter.jsx
+++ Exercices/Module2/Exercices/2.1/src/clickCounter.jsx
@@ -4,18 +4,23 @@ import { useState } from "react";
 function ClickCounter ({
     title,
     on10Clicks = "Master",
+    onMouseOver = "Please click me"
 }) {
 
     const [count, setCount] = useState(0);
+    const [isHovered, setIsHovered] = useState(false);
 
     return (
         <div className="card">
           <h4>{title}</h4>
+          {isHovered ? <p>{onMouseOver}</p> : null}
           <button
             onClick={() => {
               const newCount = count + 1;
               setCount(newCount);
             }}
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
           >
             count is {count}
           </button>
